412e7a6461282b161d85794dc98eb0026e259e66
Hải Thân Văn <thanhaibkk58@gmail.com>
2019-12-08 19:26:00 +0700

    Update profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var mongoose = require("mongoose");
 var User = require("../models/user");
 var passport = require("passport");
+var uploadImage = require("../controllers/upload_2");
 
 var checkAuthentication = require("../utils/check_authentication");
 
 /* ---------------------------------------------------- */
 /* GET - Sign up */
-router.get("/signup", function(req, res, next) {
+router.get("/signup", function(req, res) {
     res.render("dark/register.ejs", { title: "Signup" });
 });
 
@@ -19,12 +20,6 @@ router.post("/signup", passport.authenticate("local-signup", {
     failureRedirect: "/",
     failureFlash : true
 }));
-
-// Test post
-// router.post("/signup", function (req, res){
-//     console.error("-------------------------------request: " + req.body.email);
-//     res.redirect("/");
-// });
 
 // Hiển thị form login
 router.get('/login', function (req, res) {
@@ -38,6 +33,46 @@ router.post('/login', passport.authenticate("local-login", {
     failureRedirect: '/login',
     failureFlash: true
 }));
+
+// Update profile
+router.post("/user/update", checkAuthentication, function (req, res) {
+    if (!req.body) return res.status(400);
+    var avatar_url;
+    if (req.files !== null) {
+        uploadImage.uploadImage(req.files.image, req.user._id);
+        avatar_url = "http://localhost:3000/public/images/" + req.user._id + ".jpg";
+    } else {
+        avatar_url = req.user.avatar_url;
+    }
+
+    var update = {
+        email: req.body.email,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        avatar_url: avatar_url
+    };
+
+    User.findOne({email: req.body.email}, function (err, result) {
+        if (err) return res.status(400);
+        var id1;
+        if (result != null){
+            id1 = result._id + "";
+        }
+        var id2 = req.user._id + "";
+        if (id1 !== id2 && result != null){
+            // Email already exists
+            console.error("Email already exists!");
+            return res.redirect("/");
+        } else {
+            // Update
+            User.findByIdAndUpdate(req.user._id, update, function (err, user) {
+                if (err) res.status(400);
+                // return res.status(200);
+                return res.redirect("/");
+            });
+        }
+    });
+});
 
 router.get('/logout', checkAuthentication, function (req, res) {
     req.logout();
